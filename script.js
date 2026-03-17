8f959b80a8a42eb36a275a64718b74e912dc17f5
Leon Forsythe <lvforsythe95@gmail.com>
2026-03-17 15:38:12 -0400

    set up functions to view each group of chords. added js swiper functionality to chord library

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get chord diagrams and
 let chords = [];
-const library = document.getElementById("library");
+const library = document.getElementById("libraryResults");
 const eStringBarre = document.getElementById("eStringBarre")
 const aStringBarre = document.getElementById("aStringBarre")
 const dStringBarre = document.getElementById("dStringBarre")
@@ -13,13 +13,45 @@ async function fillChords() {
 
 const displayEString = async () => {
     chords = await fillChords()
-    console.log(chords)
+    library.innerHTML = ''
     let html = ''
     for (let i = 0; i < 6; i++){
-        html += `<div class="chord-card"><h2>${chords[i].name}<h2><img src="${chords[i].imgUrl}"></div>`
+        html += `<div class="chord-card swiper-slide"><h2>${chords[i].name}</h2><img src="${chords[i].imgUrl}"><button class="card-btn">Add To Favorites</button></div>`
     }
     library.innerHTML = html
+    swiper.update();
 }
+
+const displayAString = async () => {
+    chords = await fillChords()
+    library.innerHTML = ''
+    let html = ''
+    for (let i = 6; i < 12; i++){
+        html += `<div class="chord-card swiper-slide"><h2>${chords[i].name}</h2><img src="${chords[i].imgUrl}"><button class="card-btn">Add To Favorites</button></div>`
+    }
+    library.innerHTML = html
+    swiper.update();
+}
+
+const displayDString = async () => {
+    chords = await fillChords()
+    console.log(chords)
+    let html = ''
+    for (let i = 12; i < 18; i++){
+        html += `<div class="chord-card swiper-slide"><h2>${chords[i].name}</h2><img src="${chords[i].imgUrl}"><button class="card-btn">Add To Favorites</button></div>`
+    }
+    library.innerHTML = html
+    swiper.update();
+}
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+});
 
 
 eStringBarre.addEventListener("click", displayEString)
+aStringBarre.addEventListener("click", displayAString)
+dStringBarre.addEventListener("click", displayDString)
